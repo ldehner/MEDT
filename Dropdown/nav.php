f2114c7d462044aa049cceb37bf4e9c65a9a70ef
saltmaker2017 <ldehner@student.tgm.ac.at>
2017-05-13 16:02:19 +0200

    PHP

---
+++ Dropdown/nav.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<link href="php.css" rel="stylesheet" type="text/css" > 					<!-- CSS Datei -->
+	</head> 
+	<body>
+		<div id="navi">																<!-- Div Box navi -->
+<nav>																				<!-- nav bereich -->
+   		<ul class="cf">																<!-- Dropdown Beginn -->
+        <li ><a  class="dropdown" href="index.php" >Home</a> 						<!-- Home button -->
+        <li ><a class="dropdown" href="#" >Bilder</a> 								<!-- Bleibt wegen # auf der selben Seite -->
+        <li ><a class="dropdown" href="#" >Mein Bereich</a>							<!-- Oberkategorie Mein Bereich -->
+            <ul>																	<!-- Unterkategorie Anfang -->
+                <li ><a href="login.php" >Anmelden</a></li> 						<!-- Unterkategorie Mein Bereich -->
+                <li ><a href="register.php" >Registrieren</a></li>					<!-- Unterkategorie Mein Bereich -->
+                <li ><a href="vergessen.php" >Passwort vergessen?</a></li> 			<!-- Unterkategorie Mein Bereich -->
+
+            </ul>																	<!-- Unterkategorie Ende -->
+        
+        
+           <li><a class="dropdown" href="#" >Über Uns</a>
+            <ul>
+              																			
+               																		<!-- Unterkategorie Anfang -->
+                <li ><a href="kontakt.php">Kontakt</a></li>
+                <li ><a href="anfahrt.php">Anfahrt</a></li>
+                <li ><a href="partner.php">Partnerseiten</a></li>
+             
+                
+            </ul>																	<!-- Unterkategorie Ende -->
+    </ul>																			<!-- Dropdown Ende -->		
+</nav>																				<!-- nav bereich Ende -->
+		</div>
+	</body>
+</html>
